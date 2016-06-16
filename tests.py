54e49b99dfc833291f653408decc9d13257e4c2f
daegontaven <daegontaven@gmail.com>
2016-06-17 03:40:40 +0530

    Added support for nosetests
    Renamed the main function and imported nose

--- tests.py
+++ tests.py
@@ -2,16 +2,16 @@
 #coding:utf-8
 #Tests
 
+from nose.tools import *
 import filemapper as fm
 
-def main():
+def test_setup():
     print(fm.create('resources'))
     print(fm.load('resources'))
     for i in fm.read('adverbs.dat'):
         print(i)
     print(fm.close('nouns.dat'))
     print(fm.close())
-    
 
 if __name__ == "__main__":
-    main()
+    test_setup()
