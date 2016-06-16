f26f655beeefd93d745105bad09266ae8e92871b
daegontaven <daegontaven@gmail.com>
2016-06-17 04:25:31 +0530

    Testing for support on other platforms

--- filemapper/core.py
+++ filemapper/core.py
@@ -1,17 +1,22 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os, sys
+from sys import platform as _platform
 
-global mapped_files
+global mapped_files, sep
 mapped_files = {}
+sep = '/'
+
+if _platform == "win32":
+    sep = '\\'
 
 def map_files(path):
-    global mapped_files
+    global mapped_files, sep
     if sys.version_info < (3,):
         root, dirs, files = os.walk(path).next()
     else:
         root, dirs, files = os.walk(path).__next__()
     for f in files:
-        mapped_files[f]='%s\%s' % (root,f)
+        mapped_files[f]='%s%s%s' % (root,sep,f)
     return mapped_files
 
 if __name__=="__main__":
